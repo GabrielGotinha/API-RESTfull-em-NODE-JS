590d3423d8e6c4667f59fd857d4c04048298e76d
Gabirel Angello <142102428+GabrielGotinha@users.noreply.github.com>
2024-11-16 10:14:24 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+import { fastify } from "fastify";
+// import { DatabaseMemory } from "./database.js";
+import { DatabasePostgre } from "./database_postgre.js";
+
+const server = fastify();
+
+// const database = new DatabaseMemory();
+const database = new DatabasePostgre();
+
+server.post("/videos", async (request, reply) => {
+  const { title, description, duration } = request.body;
+
+  await database.create({
+    title,
+    description,
+    duration,
+  });
+
+  return reply.status(201).send();
+});
+
+server.get("/videos", async (request) => {
+  const search = request.query.search || null;
+
+  if (!search) {
+    console.log("nao tem search");
+  }
+
+  const videos = await database.list(search);
+
+  return videos;
+});
+
+server.put("/videos/:id", async (request, reply) => {
+  const videoid = request.params.id;
+
+  const { title, description, duration } = request.body;
+
+  await database.update(videoid, {
+    title,
+    description,
+    duration,
+  });
+
+  return reply.status(204).send();
+});
+
+server.delete("/videos/:id", (request, reply) => {
+  const videoid = request.params.id;
+
+  database.delete(videoid);
+
+  return reply.status(204).send();
+});
+
+server.listen({
+  port: process.env.DATABASE_URL ?? 3000,
+});
